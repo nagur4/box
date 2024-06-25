7bbea655c7a9a98f8ee9891c66beb40ea6b3c807
nagur4 <nagurbaji204@gmail.com>
2024-06-25 19:09:16 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min11.jpeg",
+    min2: "min22.jpeg",
+    min3: "min33.jpeg",
+    min4: "min44.jpeg",
+    min5: "min55.jpeg",
+    min6: "min66.jpeg",
+
+    max1: "max11.jpeg",
+    max2: "max2.jpeg",
+    max3: "max3.jpeg",
+    max4: "max4.jpeg",
+    max5: "max5.jpeg",
+    max6: "max6.jpeg",
+}
